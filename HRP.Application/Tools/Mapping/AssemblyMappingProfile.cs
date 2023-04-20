56c179459372e086c92c00dbcb7f8dcf48cc0ad9
Artem Tikhonov <artem.tikhonov@gmail.com>
2023-04-20 15:24:36 +0500

    + AutoMapper

---
+++ HRP.Application/Tools/Mapping/AssemblyMappingProfile.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using AutoMapper;
+
+namespace HRP.Application.Tools.Mapping;
+
+public class AssemblyMappingProfile : Profile
+{
+    private static readonly Dictionary<string, Type> SupportedInterfaces = new()
+    {
+        { typeof(IMapWith<>).Name, typeof(IMapWith<>) },
+        { typeof(IMapWith<,>).Name, typeof(IMapWith<,>) }
+    };
+
+    private readonly Assembly _assembly;
+
+    public AssemblyMappingProfile(Assembly assembly)
+    {
+        _assembly = assembly;
+        ApplyMapping();
+    }
+
+    private void ApplyMapping()
+    {
+        var types = _assembly.GetExportedTypes().Where(type => type.GetInterfaces()
+                .Any(i => i.IsGenericType && SupportedInterfaces.ContainsKey(i.Name)))
+            .ToList();
+
+        types.ForEach(type =>
+        {
+            var mapInterface = type.GetInterfaces().First(i => SupportedInterfaces.ContainsKey(i.Name));
+            var instance = Activator.CreateInstance(type);
+            var mapType = SupportedInterfaces[mapInterface.Name].MakeGenericType(mapInterface.GenericTypeArguments);
+            var methodInfo = mapType.GetMethod("Mapping");
+            methodInfo?.Invoke(instance, new object[] { this });
+        });
+    }
+}
---
+++ HRP.Application/Tools/Mapping/IMapWith.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+
+namespace HRP.Application.Tools.Mapping;
+
+public interface IMapWith<T>
+{
+    public void Mapping(Profile profile)
+    {
+        profile.CreateMap(typeof(T), GetType()).ReverseMap();
+    }
+}
+
+public interface IMapWith<T1, T2>
+{
+    public void Mapping(Profile profile)
+    {
+        profile.CreateMap(typeof(T1), GetType());
+        profile.CreateMap(GetType(), typeof(T2)).ReverseMap();
+    }
+}
